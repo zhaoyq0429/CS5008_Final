574b4f4047ccc10c4c6a09cc3174dfa811562bd2
mfan97 <71311372+mfan97@users.noreply.github.com>
2023-03-21 01:51:09 -0700

    added comments

--- client.c
+++ client.c
@@ -5,11 +5,20 @@
 #include <string.h>
 
 int main() {
+    // create the socket file descriptor 
+    // AF_INET: address family for IP version 4
+    // SOCK_STREAM: TCP socket
+    // 0 protocal/layers, i.e., using IP layer 
     int socketFD  = socket(AF_INET, SOCK_STREAM, 0);
+    // using google's ip as temporary server/listener 
     char* ip = "142.250.188.46";
+    // create IPv4 socket address
     const struct sockaddr_in address;
+    // set properties of address
     address.sin_family = AF_INET;
-    address.sin_port = htons(80);
+    // port # of the server that is listening HTTP connection
+    address.sin_port = htons(80); 
+    // convert to IP address from text to binary int form 
     inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
 
     int result = connect(socketFD, &address, sizeof(address));
@@ -18,8 +27,9 @@ int main() {
         printf("Connection was successful\n");
     }
 
-    char* buffer;
-    buffer = "GET \\ HTTP/1.1\r\nHOST:google.com\r\n\r\n";
-    send(socketFD, buffer, strlen(buffer), 0);
+    char* message;
+    // send to Google server using HTTP protocal 
+    message = "GET \\ HTTP/1.1\r\nHOST:google.com\r\n\r\n";
+    send(socketFD, message, strlen(message), 0);
     return 0;
 }
